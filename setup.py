806af66de782b09373b424cfebb85b5e9694d158
Parnell Springmeyer <parnell@ixmat.us>
2014-01-02 20:43:35 -0600

    Version bump and use 2to3 during install

--- setup.py
+++ setup.py
@@ -11,30 +11,31 @@ CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 setup(name='retools',
       version=__version__,
       description='Redis Tools',
-      long_description=README + '\n\n' +  CHANGES,
+      long_description=README + '\n\n' + CHANGES,
       classifiers=[
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        ],
+          "Intended Audience :: Developers",
+          "Programming Language :: Python",
+      ],
       keywords='cache redis queue lock',
       author="Ben Bangert",
       author_email="ben@groovie.org",
       url="http://readthedocs.org/docs/retools/",
       license="MIT",
       packages=find_packages(),
+      use_2to3=True,
       test_suite="retools.tests",
       include_package_data=True,
       zip_safe=False,
-      tests_require = ['pkginfo', 'Mock>=0.8rc2', 'nose',
-                       'simplejson'],
+      tests_require=['pkginfo', 'Mock>=0.8rc2', 'nose',
+                     'simplejson'],
       setup_requires=["nose"],
       install_requires=[
           "setproctitle>=1.1.2",
-          "redis>=2.7.3",
+          "redis>=2.9.0",
       ],
       entry_points="""
       [console_scripts]
       retools-worker = retools.queue:run_worker
 
       """
-)
+      )
